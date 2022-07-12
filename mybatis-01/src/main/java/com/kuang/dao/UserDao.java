0fee9d067f3fdb3b45ee7d36eaea2f7c1dd79da7
Brian <Brian@ef.cn>
2022-07-12 13:01:19 +0800

    first commit

---
+++ mybatis-01/src/main/java/com/kuang/dao/UserDao.java
@@ -0,0 +1,15 @@
+package com.kuang.dao;
+
+import com.kuang.pojo.User;
+
+import java.util.List;
+
+/**
+ * @author xxx
+ * @version 1.0
+ * @Description
+ * @date 2022/7/11 23:33
+ */
+public interface UserDao {
+    List<User> getUserList();
+}
---
+++ mybatis-01/src/test/java/com/kuang/dao/UserDaoTest.java
@@ -0,0 +1,36 @@
+package com.kuang.dao;
+
+
+import com.kuang.utils.MybatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+public class UserDaoTest {
+    @Test
+    public void test(){
+        SqlSession sqlSession = null;
+        try {
+            //第一步：获得SqlSession对象
+            sqlSession = MybatisUtils.getSqlSession();
+
+
+            //方式一：getMapper
+            UserDao userDao = sqlSession.getMapper(UserDao.class);
+            List<pojo.User> userList = userDao.getUserList();
+
+            //方式二：
+            //List<pojo.User> userList = sqlSession.selectList("com.kuang.dao.UserDao.getUserList");
+
+            for (pojo.User user : userList) {
+                System.out.println(user);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            //关闭SqlSession
+            sqlSession.close();
+        }
+    }
+}
