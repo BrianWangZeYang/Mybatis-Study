dd1e9bf572bced582469cea36bf863377996fbfb
Brian <Brian@ef.cn>
2022-07-14 12:42:18 +0800

    Before the optimization commit

---
+++ mybatis-02/src/test/java/com/kuang/dao/UserDaoTest.java
@@ -0,0 +1,29 @@
+package com.kuang.dao;
+
+
+import com.kuang.pojo.User;
+import com.kuang.utils.MybatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class UserDaoTest {
+    @Test
+    public void getUserLike(){
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = MybatisUtils.getSqlSession();
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+            List<User> userList = mapper.getUserList();
+            for (User user: userList) {
+                System.out.println(user);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            sqlSession.close();
+        }
+    }
+}
