2e0175dfde4534a11898ceb40068c9b3ad500170
Brian <Brian@ef.cn>
2022-07-15 12:44:09 +0800

    resultMap commit

---
+++ mybatis-03/src/test/java/com/kuang/dao/UserDaoTest.java
@@ -0,0 +1,21 @@
+package com.kuang.dao;
+
+
+import com.kuang.pojo.User;
+import com.kuang.utils.MybatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+public class UserDaoTest {
+    @Test
+    public void getUserById(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User userById = mapper.getUserById(1);
+        System.out.println(userById);
+
+        sqlSession.close();
+
+    }
+}
