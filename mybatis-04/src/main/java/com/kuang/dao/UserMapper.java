a9e352384a7c125586ff85865790199ae23696f8
Brian <Brian@ef.cn>
2022-07-16 16:00:21 +0800

    limit commit

--- mybatis-04/src/main/java/com/kuang/dao/UserMapper.java
+++ mybatis-04/src/main/java/com/kuang/dao/UserMapper.java
@@ -3,26 +3,13 @@ package com.kuang.dao;
 import com.kuang.pojo.User;
 
 import java.util.List;
+import java.util.Map;
 
-/**
- * @author xxx
- * @version 1.0
- * @Description
- * @date 2022/7/11 23:33
- */
 public interface UserMapper {
-    //查询全部用户
-    List<User> getUserList();
 
     //根据ID查询用户
     User getUserById(int id);
 
-    //insert一个用户
-    int insertUser(User user);
-
-    //修改一个用户
-    int updateUser(User user);
-
-    //删除一个用户
-    int deleteUser(int id);
+    //分页
+    List<User> getUserByLimit(Map<String,Integer> map);
 }
--- mybatis-04/src/test/java/com/kuang/dao/UserDaoTest.java
+++ mybatis-04/src/test/java/com/kuang/dao/UserDaoTest.java
@@ -7,8 +7,31 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class UserDaoTest {
     static Logger logger = Logger.getLogger(UserMapper.class);
+
+    @Test
+    public void getUserByLimit(){
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = MybatisUtils.getSqlSession();
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+            HashMap<String, Integer> map = new HashMap<>();
+            map.put("startIndex",0);
+            map.put("pageSize",2);
+            List<User> userByLimit = mapper.getUserByLimit(map);
+            for (User user : userByLimit) {
+                System.out.println(user);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            sqlSession.close();
+        }
+    }
     @Test
     public void getUserById(){
         SqlSession sqlSession = MybatisUtils.getSqlSession();
@@ -19,7 +42,6 @@ public class UserDaoTest {
         System.out.println(userById);
 
         sqlSession.close();
-
     }
     @Test
     public void tsetLog4j(){
