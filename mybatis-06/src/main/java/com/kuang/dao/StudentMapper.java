a7246246f2656abca315361d443231c58352d304
Brian <Brian@ef.cn>
2022-07-19 23:51:04 +0800

    many for one commit

--- mybatis-06/src/main/java/com/kuang/dao/StudentMapper.java
+++ mybatis-06/src/main/java/com/kuang/dao/StudentMapper.java
@@ -1,5 +1,9 @@
 package com.kuang.dao;
 
+import com.kuang.pojo.Student;
+
+import java.util.List;
+
 /**
  * @author xxx
  * @version 1.0
@@ -7,5 +11,7 @@ package com.kuang.dao;
  * @date 2022/7/18 22:35
  */
 public interface StudentMapper {
-
+    //查询所有的学生信息以及对应老师的信息。
+    public List<Student> getStudent();
+    public List<Student> getStudent2();
 }
--- mybatis-06/src/test/java/UserMapperTest.java
+++ mybatis-06/src/test/java/UserMapperTest.java
@@ -1,8 +1,12 @@
+import com.kuang.dao.StudentMapper;
 import com.kuang.dao.TeacherMapper;
+import com.kuang.pojo.Student;
 import com.kuang.pojo.Teacher;
 import com.kuang.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
+
+import java.util.List;
 
 public class UserMapperTest {
 
@@ -20,5 +24,21 @@ public class UserMapperTest {
         }
         sqlSession.close();
 
+    }
+    @Test
+    public void testStudent(){
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = MybatisUtils.getSqlSession();
+            StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
+            List<Student> student = mapper.getStudent2();
+            for (Student student1 : student) {
+                System.out.println(student1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+        }
+        sqlSession.close();
     }
 }
